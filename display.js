ef85859395495faf8393aa22d774c5c3d17d33b4
BuildTools <unconfigured@null.spigotmc.org>
2024-04-01 10:34:14 +0200

    Moving js scripts into 'scripts' directory.

--- display.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  setInterval(function(){
-    $('#team1Name').html(localStorage.getItem('team1Name'));
-    $('#team1Points').html(localStorage.getItem('team1Points'));
-    $('#team2Name').html(localStorage.getItem('team2Name'));
-    $('#team2Points').html(localStorage.getItem('team2Points'));
-    $('#timer').html(localStorage.getItem('timer'));
-  }, 200);
-});
